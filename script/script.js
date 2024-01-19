395fc5f26604bad3c797c46087e76602917b3765
Uliya Zayceva <juliyazayceva@mail.ru>
2024-01-19 17:30:17 +0800

    refactor: add classes for margins and div "page"

--- script/script.js
+++ script/script.js
@@ -1,15 +1,18 @@
-let profile = document.querySelector('.profile');
-let popup = document.querySelector('.popup');
+let body = document.querySelector('.body')
+let profile = body.querySelector('.profile');
+let popup = body.querySelector('.popup');
 
 let editButton = profile.querySelector('.profile__edit-button');
 let closeButton = popup.querySelector('.popup__close-button')
 
 function openPopup () {
-  popup.classList.add('popup_opened')
+  popup.classList.add('popup_opened');
+  body.classList.add('body_no-scroll');
 }
 
 function closePopup () {
-  popup.classList.remove('popup_opened')
+  popup.classList.remove('popup_opened');
+  body.classList.remove('body_no-scroll')
 }
 
 editButton.addEventListener('click', openPopup);
